7eb6cfe94b7e79a87d8e9647be7288f4af5756a7
Brk <kazancibulent@gmail.com>
2017-12-13 17:10:03 +0300

    implemented asynchronous content tagging.

--- src/main/java/com/zenith/livinghistory/api/zenithlivinghistoryapi/common/SparQL/MagicalStrings.java
+++ src/main/java/com/zenith/livinghistory/api/zenithlivinghistoryapi/common/SparQL/MagicalStrings.java
@@ -6,6 +6,5 @@ public final class MagicalStrings {
     // SparQL strings.
     public static String DbpediaURL = "http://dbpedia.org/sparql";
     public static String QueryTimeout = "10000";
-    public static String CurryPrefix = "PREFIX";
 
 }
